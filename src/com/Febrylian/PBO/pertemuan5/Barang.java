ed8690d45e402473b4654727440d6e9f13b6228f
Nailyr <febrylian123@gmail.com>
2023-05-03 13:40:56 +0700

    Pertemuan 5

---
+++ src/com/Febrylian/PBO/pertemuan5/Barang.java
@@ -0,0 +1,18 @@
+package com.Febrylian.PBO.pertemuan5;
+
+public class Barang {
+    String nama;
+    int harga;
+
+    public Barang(){}
+
+    public Barang(String nama, int harga){
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public void showInfo(){
+        System.out.println("Nama : " + nama);
+        System.out.println("Harga : " + harga);
+    }
+}
