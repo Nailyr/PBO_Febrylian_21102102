fb6b31d3dd9105ea81fec4b29f3c6320a35ba512
Nailyr <febrylian123@gmail.com>
2023-05-31 14:08:11 +0700

    Pertemuan 5

---
+++ src/com/Febrylian/PBO/pertemuan7/Registration.java
@@ -0,0 +1,19 @@
+package com.Febrylian.PBO.pertemuan7;
+
+import javax.swing.*;
+
+public class Registration {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JTextField textField3;
+    private JRadioButton lakiLakiRadioButton;
+    private JRadioButton perempuanRadioButton;
+    private JTextArea textArea1;
+    private JButton saveButton;
+    private JButton clearButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
